86e80164623bf5e904524226dd8f1c2d680e0d45
김동관 <@DESKTOP-QL6HG8E>
2021-08-24 09:39:18 +0900

    .

---
+++ junitEX/src/junitEX/test/TargetClassTest.java
@@ -0,0 +1,62 @@
+package junitEX.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+import junitTarget.TargetClass;
+
+public class TargetClassTest {
+
+	private TargetClass classUnderTest;
+	
+	@Rule
+	public TestName testName = new TestName();
+
+	@Before
+	public void setup() {
+		classUnderTest = new TargetClass();
+		System.out.println("Start " + testName.getMethodName());
+	}
+
+	@Test
+	public void test_doubleANumber() {
+		
+		//Test the doubleANumber() method
+		
+	}
+
+	@Test
+	public void test_returnBooleanFoo_false() {
+		
+		//Test the returnABoolean() method with assertFalse
+	}
+
+	@Test
+	public void test_returnBooleanFoo_true() {
+		
+		//Test the returnABoolean() method with assertTrue
+
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void test_voidFoo_exception() throws IllegalAccessException {
+
+		//Test the voidFoo() method with assertTrue
+		
+	}
+	
+	@Test(timeout = 1)
+	public void test_timeout() {
+		
+		//Test the doubleANumber()		
+		
+	}
+}
---
+++ junitEX/src/junitTarget/TargetClass.java
@@ -0,0 +1,24 @@
+package junitTarget;
+
+public class TargetClass {    
+	  
+    public int doubleANumber(int num) {
+        return num * 2;
+    }
+     
+    public boolean returnABoolean(String inputData) {
+        if ("Save".equalsIgnoreCase(inputData)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+ 
+    public void voidFoo(String inputData) {
+        if ("Ok".equalsIgnoreCase(inputData)) {
+            System.out.println("doing something.");;
+        } else {
+            throw new IllegalArgumentException("Bad argument:" + inputData);
+        }
+    }
+}
