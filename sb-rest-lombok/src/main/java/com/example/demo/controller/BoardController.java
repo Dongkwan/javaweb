b8baaeff60382055c4dea6cc5d3508906e95bccd
김동관 <@DESKTOP-QL6HG8E>
2021-10-13 08:53:02 +0900

    sb, rest controller, lombok

---
+++ sb-rest-lombok/src/main/java/com/example/demo/controller/BoardController.java
@@ -0,0 +1,55 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.BoardVO;
+//import com.example.demo.domain.BoardVO2;
+
+@RestController
+public class BoardController {
+
+	public BoardController() {
+		System.out.println("===> BoardController 생성");
+	}
+	
+	@GetMapping("/hello")
+	public String hello(String name) {
+		return "Hello : " + name;
+	}
+	
+	//VO 객체를 클라이언트에 전달
+	@GetMapping("/getBoard")
+	public BoardVO getBoard() {
+		BoardVO board = new BoardVO();
+		board.setSeq(1);
+		board.setTitle("테스트 제목...");
+		board.setWriter("테스터");
+		board.setContent("테스트 내용입니다...............");
+		board.setCreateDate(new Date());
+		board.setCnt(0);
+		return board;
+	}
+	
+	//VO 객체의 컬렉션을 클라이언트에 전달
+	@GetMapping("/getBoardList")
+	public List<BoardVO> getBoardList() {
+		List<BoardVO> boardList = new ArrayList<BoardVO>();
+		for (int i = 1; i <= 10; i++) {
+			BoardVO board = new BoardVO();
+			board.setSeq(i);
+			board.setTitle("제목" + i);
+			board.setWriter("테스터");
+			board.setContent(i + "번 내용입니다.");
+			board.setCreateDate(new Date());
+			board.setCnt(0);
+			boardList.add(board);
+		}
+		return boardList;
+	}
+
+}
