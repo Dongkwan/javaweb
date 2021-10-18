16dd6eb2ba3261a096687eafbf461af2e63f4a4c
김동관 <@DESKTOP-QL6HG8E>
2021-10-18 18:15:15 +0900

    sb, thymeleaf, get-started

---
+++ sb-thymeleaf-get-started/src/main/java/sb/thymeleaf/get/started/SbThymeleafGetStartedApplication.java
@@ -0,0 +1,13 @@
+package sb.thymeleaf.get.started;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbThymeleafGetStartedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbThymeleafGetStartedApplication.class, args);
+	}
+
+}
---
+++ sb-thymeleaf-get-started/src/test/java/sb/thymeleaf/get/started/SbThymeleafGetStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package sb.thymeleaf.get.started;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbThymeleafGetStartedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
